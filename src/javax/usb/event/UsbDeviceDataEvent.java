81528e3f40fbdf324ac9eef5b0b5517f67e76a5d
Dan Streetman <ddstreet@us.ibm.com>
2003-01-06 16:33:25 +0000

    change UsbIrp to ControlUsbIrp

--- src/javax/usb/event/UsbDeviceDataEvent.java
+++ src/javax/usb/event/UsbDeviceDataEvent.java
@@ -27,15 +27,15 @@ public class UsbDeviceDataEvent extends UsbDeviceEvent
 	/**
 	 * Constructor.
 	 * @param source The UsbDevice.
-	 * @param irp The UsbIrp, or null if no UsbIrp was involved.
+	 * @param irp The ControlUsbIrp.
 	 * @param data The data.
 	 * @param length The amount of data transferred.
 	 * @exception IllegalArgumentException If the length is less than 0 or greater than data.length.
 	 */
-	public UsbDeviceDataEvent( UsbDevice source, UsbIrp irp, byte[] data, int length ) throws IllegalArgumentException
+	public UsbDeviceDataEvent( UsbDevice source, ControlUsbIrp irp, byte[] data, int length ) throws IllegalArgumentException
 	{
 		super(source);
-		usbIrp = irp;
+		controlUsbIrp = irp;
 		if (0 > length || length > data.length)
 			throw new IllegalArgumentException("Illegal data length " + length + "; length must be between 0 and data.length");
 		this.data = new byte[length];
@@ -43,12 +43,10 @@ public class UsbDeviceDataEvent extends UsbDeviceEvent
 	}
 
 	/**
-	 * Get the UsbIrp.
-	 * <p>
-	 * If no UsbIrp was involved, this returns null.
-	 * @return The UsbIrp, or null.
+	 * Get the ControlUsbIrp.
+	 * @return The ControlUsbIrp.
 	 */
-	public UsbIrp getUsbIrp() { return usbIrp; }
+	public ControlUsbIrp getControlUsbIrp() { return controlUsbIrp; }
 
 	/**
 	 * Get the data.
@@ -59,7 +57,7 @@ public class UsbDeviceDataEvent extends UsbDeviceEvent
 	 */
 	public byte[] getData() { return data; }
 
-	private UsbIrp usbIrp = null;
+	private ControlUsbIrp controlUsbIrp = null;
 	private byte[] data = null;
 
 }
