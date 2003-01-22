6d6e848bed2c53a612b9d7302a356aaf2b80511a
Dan Streetman <ddstreet@us.ibm.com>
2003-01-22 22:00:53 +0000

    CONFIG -> CONFIGURATION changed tabs to space

--- src/javax/usb/UsbConst.java
+++ src/javax/usb/UsbConst.java
@@ -30,9 +30,9 @@ public interface UsbConst
 	//**************************************************************************
 	// Configuration constants
 
-    public static final byte CONFIG_POWERED_MASK  = (byte)0x60;
-    public static final byte CONFIG_SELF_POWERED  = (byte)0x40;
-    public static final byte CONFIG_REMOTE_WAKEUP = (byte)0x20;
+    public static final byte CONFIGURATION_POWERED_MASK  = (byte)0x60;
+    public static final byte CONFIGURATION_SELF_POWERED  = (byte)0x40;
+    public static final byte CONFIGURATION_REMOTE_WAKEUP = (byte)0x20;
 
 	//**************************************************************************
 	// Endpoint constants
@@ -49,17 +49,17 @@ public interface UsbConst
     public static final byte ENDPOINT_TYPE_BULK                         = (byte)0x02;
     public static final byte ENDPOINT_TYPE_INTERRUPT                    = (byte)0x03;
 
-	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_MASK		    = (byte)0x0c;
-	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_NONE		    = (byte)0x00;
+	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_MASK         = (byte)0x0c;
+	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_NONE         = (byte)0x00;
 	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_ASYNCHRONOUS = (byte)0x04;
-	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_ADAPTIVE	    = (byte)0x08;
+	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_ADAPTIVE     = (byte)0x08;
 	public static final byte ENDPOINT_SYNCHRONIZATION_TYPE_SYNCHRONOUS  = (byte)0x0c;
 
-	public static final byte ENDPOINT_USAGE_TYPE_MASK				    = (byte)0x30;
-	public static final byte ENDPOINT_USAGE_TYPE_DATA				    = (byte)0x00;
-	public static final byte ENDPOINT_USAGE_TYPE_FEEDBACK			    = (byte)0x10;
+	public static final byte ENDPOINT_USAGE_TYPE_MASK                   = (byte)0x30;
+	public static final byte ENDPOINT_USAGE_TYPE_DATA                   = (byte)0x00;
+	public static final byte ENDPOINT_USAGE_TYPE_FEEDBACK               = (byte)0x10;
 	public static final byte ENDPOINT_USAGE_TYPE_IMPLICIT_FEEDBACK_DATA = (byte)0x20;
-	public static final byte ENDPOINT_USAGE_TYPE_RESERVED			    = (byte)0x30;
+	public static final byte ENDPOINT_USAGE_TYPE_RESERVED               = (byte)0x30;
 
 	//**************************************************************************
 	// Request constants
@@ -75,7 +75,7 @@ public interface UsbConst
 	public static final byte REQUESTTYPE_TYPE_RESERVED       = (byte)0x60;
 
 	public static final byte REQUESTTYPE_RECIPIENT_MASK      = (byte)0x1f;
-	public static final byte REQUESTTYPE_RECIPIENT_DEVICE  	 = (byte)0x00;
+	public static final byte REQUESTTYPE_RECIPIENT_DEVICE    = (byte)0x00;
 	public static final byte REQUESTTYPE_RECIPIENT_INTERFACE = (byte)0x01;
 	public static final byte REQUESTTYPE_RECIPIENT_ENDPOINT  = (byte)0x02;
 	public static final byte REQUESTTYPE_RECIPIENT_OTHER     = (byte)0x03;
@@ -95,17 +95,17 @@ public interface UsbConst
 	//**************************************************************************
 	// Descriptor constants
 
-    public static final byte DESCRIPTOR_TYPE_DEVICE          = (byte)0x01;
-    public static final byte DESCRIPTOR_TYPE_CONFIG          = (byte)0x02;
-    public static final byte DESCRIPTOR_TYPE_STRING          = (byte)0x03;
-    public static final byte DESCRIPTOR_TYPE_INTERFACE       = (byte)0x04;
-    public static final byte DESCRIPTOR_TYPE_ENDPOINT        = (byte)0x05;
+	public static final byte DESCRIPTOR_TYPE_DEVICE              = (byte)0x01;
+	public static final byte DESCRIPTOR_TYPE_CONFIGURATION       = (byte)0x02;
+	public static final byte DESCRIPTOR_TYPE_STRING              = (byte)0x03;
+	public static final byte DESCRIPTOR_TYPE_INTERFACE           = (byte)0x04;
+	public static final byte DESCRIPTOR_TYPE_ENDPOINT            = (byte)0x05;
 
-    public static final byte DESCRIPTOR_MIN_LENGTH           = (byte)0x02;
-    public static final byte DESCRIPTOR_MIN_LENGTH_DEVICE    = (byte)0x12;
-    public static final byte DESCRIPTOR_MIN_LENGTH_CONFIG    = (byte)0x09;
-    public static final byte DESCRIPTOR_MIN_LENGTH_INTERFACE = (byte)0x09;
-    public static final byte DESCRIPTOR_MIN_LENGTH_ENDPOINT  = (byte)0x07;
-    public static final byte DESCRIPTOR_MIN_LENGTH_STRING    = (byte)0x02;
+	public static final byte DESCRIPTOR_MIN_LENGTH               = (byte)0x02;
+	public static final byte DESCRIPTOR_MIN_LENGTH_DEVICE        = (byte)0x12;
+	public static final byte DESCRIPTOR_MIN_LENGTH_CONFIGURATION = (byte)0x09;
+	public static final byte DESCRIPTOR_MIN_LENGTH_INTERFACE     = (byte)0x09;
+	public static final byte DESCRIPTOR_MIN_LENGTH_ENDPOINT      = (byte)0x07;
+	public static final byte DESCRIPTOR_MIN_LENGTH_STRING        = (byte)0x02;
 
 }
