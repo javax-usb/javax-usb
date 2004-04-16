60c5c19b5b0b2537e07010997936460f0b154b22
Dan Streetman <ddstreet@us.ibm.com>
2004-04-16 19:59:27 +0000

    removed deprecated constructor

--- src/javax/usb/event/UsbDeviceErrorEvent.java
+++ src/javax/usb/event/UsbDeviceErrorEvent.java
@@ -23,18 +23,6 @@ public class UsbDeviceErrorEvent extends UsbDeviceEvent
 	/**
 	 * Constructor.
 	 * @param source The UsbDevice.
-	 * @param uE The UsbException.
-	 * @deprecated This must not be called, the UsbControlIrp must be provided.
-	 */
-	public UsbDeviceErrorEvent( UsbDevice source, UsbException uE )
-	{
-		super( source ); /* required to compile */
-		throw new RuntimeException("This constructor is deprecated and must not be used");
-	}
-
-	/**
-	 * Constructor.
-	 * @param source The UsbDevice.
 	 * @param irp The UsbControlIrp associated with this error.
 	 */
 	public UsbDeviceErrorEvent( UsbDevice source, UsbControlIrp irp )
